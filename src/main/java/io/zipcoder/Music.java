84be419f7d65c9e422c27325150961e4d58d91da
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-15 15:37:31 -0400

    Lab finished to completion

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,33 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
+        int nextSongCount = 0;
+        int lastSongCount = 0;
+
+        for(int i = startIndex; i <= playList.length; i++){
+            if(i == playList.length){
+                i = 0;
+            }
+            nextSongCount++;
+            if(playList[i] == selection){
+                break;
+            }
+        }
+        for(int j = startIndex; j > -1; j--){
+            if(j == -1){
+                j = playList.length - 1;
+            }
+            lastSongCount++;
+            if(playList[j] == selection){
+                break;
+            }
+        }
+
+        if(nextSongCount < lastSongCount){
+            return nextSongCount;
+        } else if(lastSongCount < nextSongCount){
+            return lastSongCount;
+        }
         return null;
     }
 }
